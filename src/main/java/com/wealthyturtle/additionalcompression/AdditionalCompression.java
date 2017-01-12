c6708d45ea16c4ab82fcafcdbd4cb817f2f8ba75
RCXcrafter <rcxcrafter@gmail.com>
2017-01-12 15:22:02 +0100

    Increase version
    It's 2.0 now in case you were wondering.

--- src/main/java/com/wealthyturtle/additionalcompression/AdditionalCompression.java
+++ src/main/java/com/wealthyturtle/additionalcompression/AdditionalCompression.java
@@ -31,7 +31,7 @@ public class AdditionalCompression {
     //@SidedProxy(clientSide = "com.wealthyturtle.additionalcompression.proxy.ClientProxy", serverSide = "com.wealthyturtle.additionalcompression.proxy.CommonProxy")
     //public static CommonProxy proxy;
 	public static final String MODID = "additionalcompression";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "2.0";
 
 	public static final CreativeTabs creativeTabs = new CreativeTabs(MODID) {
 		@SideOnly(Side.CLIENT)
